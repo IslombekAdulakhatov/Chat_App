578a0785f3446748fef532dbee0d75eb9b132e4e
IslombekAdulakhatov <36397876+IslombekAdulakhatov@users.noreply.github.com>
2022-04-19 22:02:53 -0700

    Chat App

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,16 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Islombek Abdulakhatov on 13/01/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+
+struct Message {
+    let sender : String
+    
+    let body: String
+}
